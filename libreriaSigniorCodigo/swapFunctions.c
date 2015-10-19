5f821efb6440d28a911430a70b0f0769eb2cad29
Eduardo Beghe <edubeghe@gmail.com>
2015-10-18 22:02:24 -0300

    Swap casi terminada, solamente falta adecuar como se reciben los mensajes e interpretan

--- libreriaSigniorCodigo/swapFunctions.c
+++ libreriaSigniorCodigo/swapFunctions.c
@@ -8,6 +8,7 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -19,20 +20,40 @@
 #include "swapFunctions.h"
 #include "libSockets.h"
 
+
 int doesFileExist(const char *filename) {
 	FILE *fp = fopen (filename, "r");
    if (fp!=NULL) fclose (fp);
    return (fp!=NULL);
 }
 
-void setupSwap(){
-	//Traigo datos del archivo de configuracion de Swap y creo un archivo
-	//con las especificaciones de configuracion con /0
+int getSwapPagesAmount(){
 	t_config *swapConfiguracion;
 	swapConfiguracion = config_create("/home/utnso/git/tp-2015-2c-signiorcodigo/swap/swapConfig");
 	int pages_amount = config_get_int_value(swapConfiguracion,"CANTIDAD_PAGINAS");
+	return pages_amount;
+}
+
+int getSwapPagesSize(){
+	t_config *swapConfiguracion;
+	swapConfiguracion = config_create("/home/utnso/git/tp-2015-2c-signiorcodigo/swap/swapConfig");
 	int page_size = config_get_int_value(swapConfiguracion,"TAMANIO_PAGINA");
+	return page_size;
+}
+
+char* getSwapFileName(){
+	t_config *swapConfiguracion;
+	swapConfiguracion = config_create("/home/utnso/git/tp-2015-2c-signiorcodigo/swap/swapConfig");
 	char* file_name = config_get_string_value(swapConfiguracion,"NOMBRE_SWAP");
+	return file_name;
+}
+
+void setupSwap(){
+	//Traigo datos del archivo de configuracion de Swap y creo un archivo
+	//con las especificaciones de configuracion con /0
+	int pages_amount = getSwapPagesAmount();
+	int page_size = getSwapPagesSize();
+	char *file_name = getSwapFileName();
 	char command[100];
 	int total = pages_amount * page_size;
 	sprintf(command, "dd if=/dev/zero of=%s bs=%d count=1",file_name,total );
@@ -40,18 +61,157 @@ void setupSwap(){
 	printf("Archivo de swap creado bajo el nombre %s\n",file_name );
 }
 
-t_list* getPages(int pagesAmount){
+void listDestroy(t_list *pages){
+	int size = list_size(pages);
+	t_nodo_swap* item = NULL;
+	int i;
+	for (i = 0; i < size ; i++){
+		item = (t_nodo_swap*) list_get(pages,i);
+		free(item);
+	}
+	list_destroy(pages);
+}
+
+t_list* setPages(int pagesAmount){
 	t_list *paginas = list_create();
 	int i;
 	int top = pagesAmount+1;
 	for (i = 1; i < top ; i++){
 		t_nodo_swap *newItemPtr = malloc(sizeof(t_nodo_swap));
-		t_nodo_swap newItem;
-		newItemPtr = &newItem;
-		newItem.numeroPagina = i;
-		newItem.nombreProceso = "";
+		newItemPtr->numeroPagina = i;
+		newItemPtr->nombreProceso = "";
 	  	list_add(paginas,newItemPtr);
 	}
 	return paginas;
 }
+
+void markPage(int pageNumber,char *processName, t_list *pages){
+	t_nodo_swap* item = (t_nodo_swap*) list_get(pages,pageNumber);
+	item->nombreProceso = processName;
+}
+
+void writePage(int pageNumber,char *processName,char *content,t_list *pages){
+	markPage(pageNumber,processName,pages);
+	int page_size = getSwapPagesSize();
+	char *file_name = getSwapFileName();
+	FILE *fp=fopen(file_name, "rb+");
+	int position = pageNumber * page_size;
+	fseek( fp, position, SEEK_SET);
+	fwrite(content,sizeof(char),strlen(content),fp);
+	fclose(fp);
+}
+
+char* readPage(int pageNumber){
+	int page_size = getSwapPagesSize();
+	char *file_name = getSwapFileName();
+	FILE *fp=fopen(file_name, "r");
+	char* page = malloc(sizeof(char)*page_size);
+	int position = pageNumber * page_size;
+	fseek( fp, position, SEEK_SET);
+	fgets(page,page_size+1,fp);
+	return page;
+}
+
+void evaluateAction(char* action, t_list *pages){
+	if(strcmp(action,"reservar")==0){
+		printf("Ingrese nombre de Proceso\n");
+		char *process_name = malloc(sizeof(char)*15);
+		scanf ("%s",process_name);
+		printf("Ingrese cantidad de paginas a reservar\n");
+		int amount_to_reserve;
+		scanf ("%d",&amount_to_reserve);
+		reserve(process_name,amount_to_reserve, pages);
+	}else if(strcmp(action,"escribir")==0){
+		printf("Ingrese nombre de Proceso\n");
+		char *process_name = malloc(sizeof(char)*15);
+		scanf ("%s",process_name);
+		int hasReservedPages;
+		hasReservedPages = checkProcessSpace(process_name, pages);
+		if (hasReservedPages>=0){
+			char *text = "texto de prueba para signior funti";
+			int start = checkProcessSpace(process_name,pages);
+			writePage(start-1,process_name,text,pages);
+		} else {
+			printf("El proceso no posee páginas reservadas\n");
+		}
+	}else if(strcmp(action,"leer")==0){
+		printf("Ingrese pagina de lectura\n");
+		int pagina_lectura;
+		scanf ("%d",&pagina_lectura);
+		char *retorno = readPage(pagina_lectura);
+		printf("%s\n",retorno);
+	}else if(strcmp(action,"recorrer")==0){
+		checkProcessSpace("prueba",pages);
+	}else if(strcmp(action,"liberar")==0){
+		printf("Ingrese nombre de Proceso\n");
+		char *process_name = malloc(sizeof(char)*15);
+		scanf ("%s",process_name);
+		freeSpace(process_name,pages);
+	}else{
+		printf("Accion desconocida\n");
+	}
+}
+
+int checkSpaceAvailability (int amount, t_list *pages){
+	int top = list_size(pages);
+	int count = 0;
+	int i;
+	if (amount<=list_size(pages)){
+		for (i = 0; i < top ; i++){
+			t_nodo_swap *newItemPtr = (t_nodo_swap*)list_get(pages,i);
+			if (strcmp(newItemPtr->nombreProceso,"")==0){
+				count++;
+			} else {
+				count = 0;
+			}
+			printf("%d\n",count);
+			if (count == amount){
+				return (i-amount+1);
+			}
+		}
+	}
+	return -1;
+}
+
+void freeSpace(char *name,t_list *pages){
+	int top = list_size(pages);
+	int i;
+	for (i = 0; i < top ; i++){
+		t_nodo_swap *newItemPtr = (t_nodo_swap*)list_get(pages,i);
+		if (strcmp(newItemPtr->nombreProceso,name)==0){
+			newItemPtr->nombreProceso = "";
+			printf("%d pagina liberada\n",i);
+		}
+	}
+}
+
+void reserve(char* name, int amount, t_list *pages){
+	int result = checkSpaceAvailability(amount,pages);
+	printf("%d\n",result);
+	if (result>=0){
+		int i;
+		int top = result + amount + 1;
+		for (i = result; i < top ; i++){
+			markPage(i,name,pages);
+		}
+		printf("Paginas reservadas\n");
+	} else {
+		printf("No existe espacio suficiente para el Proceso\n");
+	}
+}
+
+int checkProcessSpace(char* name, t_list *pages){
+	int i;
+	int pagesAmount = getSwapPagesAmount();
+	int top = pagesAmount;
+	t_nodo_swap *newItemPtr = malloc(sizeof(t_nodo_swap));
+	for (i = 0; i < top ; i++){
+		newItemPtr = (t_nodo_swap*)list_get(pages,i);
+		if (strcmp(newItemPtr->nombreProceso,name)==0){
+			return newItemPtr->numeroPagina;
+		}
+	}
+	return -1;
+}
+
 
--- libreriaSigniorCodigo/swapFunctions.h
+++ libreriaSigniorCodigo/swapFunctions.h
@@ -8,15 +8,45 @@
 #ifndef SWAPFUNCTIONS_H_
 #define SWAPFUNCTIONS_H_
 
-void setupSwap();
-
-int doesFileExist(const char *filename);
-
-t_list* getPages(int pagesAmount);
+//Estructuras
 
 typedef struct {
 	int numeroPagina;
 	char *nombreProceso;
 } t_nodo_swap;
+
+
+//Metodos
+
+
+void setupSwap();
+
+void listDestroy(t_list *pages);
+
+int doesFileExist(const char *filename);
+
+t_list* setPages(int pagesAmount);
+
+int getSwapPagesAmount();
+
+int getSwapPagesSize();
+
+char* getSwapFileName();
+
+void markPage(int pageNumber,char *processName, t_list *pages);
+
+void writePage(int pageNumber,char *processName,char *content,t_list *pages);
+
+char* readPage(int pageNumber);
+
+void evaluateAction(char* action, t_list *pages);
+
+void reserve(char* name, int amount, t_list *pages);
+
+int checkProcessSpace(char* name, t_list *pages);
+
+int checkSpaceAvailability(int amount, t_list *page);
+
+void freeSpace(char *name,t_list *pages);
 
 #endif /* SWAPFUNCTIONS_H_ */
